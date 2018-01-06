c4c61626da4b44ffe8c4911c0b108b7cf46b4627
lWarne <laurencewarne@gmail.com>
2018-01-06 17:56:35 +0000

    Create project skeleton using maven 'archetype:generate'.

---
+++ src/main/java/com/gmail/laurencewarne/artgenerator/App.java
@@ -0,0 +1,13 @@
+package com.gmail.laurencewarne.artgenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
